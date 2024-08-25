46954b81aa77f8774b2cd030894b77746a0cee7a
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2024-08-25 09:38:16 +0000

    url

--- client/content.js
+++ client/content.js
@@ -6,7 +6,7 @@ let iamhost = false;
 let allusersinroom = [];
 
 /* Server URL */
-const socket = io("http://localhost:4000/");
+const socket = io("https://wewatch-5wj7.onrender.com");
 
 socket.on("whoami", ({ id }) => {
   // console.log('myid', id);
