7148a79438f70823d20e6ba1ad5e674613a7bbd1
Full-Stack-boi <151743553+Full-Stack-boi@users.noreply.github.com>
2024-08-25 18:39:08 +0700

    reolls

--- server.js
+++ server.js
@@ -11,29 +11,33 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  //   socket.emit("whoami", { id: socket.id });
-  //   // join to the room
-  //   socket.on("joinmetothisroom", ({ roomid, name }) => {
-  //     socket.join(roomid);
-  //     socket.emit("joinmetothisroomsuccess", `${roomid} `);
-  //     io.to(roomid).emit("someonejoined", name);
-  //   });
-  //   // tell everyone who are here in the room
-  //   socket.on("tell_everyone_who_joined", ({ allusers, roomid }) => {
-  //     io.to(roomid).emit("who_joined", allusers);
-  //   });
-  //   // check connection
-  //   socket.on("msg", ({ data, roomid }) => {
-  //     io.to(roomid).emit("msg", data);
-  //   });
-  //   // get video state
-  //   socket.on("videoStates", ({ videoState, roomid }) => {
-  //     io.to(roomid).emit("videoStates", videoState);
-  //   });
-  //   // disconnect
-  //   socket.on("disconnect", () => {
-  //     console.log("user disconnected");
-  //   });
+  socket.emit("whoami", { id: socket.id });
+  // join to the room
+  socket.on("joinmetothisroom", ({ roomid, name }) => {
+    socket.join(roomid);
+    socket.emit("joinmetothisroomsuccess", `${roomid} `);
+    io.to(roomid).emit("someonejoined", name);
+  });
+
+  // tell everyone who are here in the room
+  socket.on("tell_everyone_who_joined", ({ allusers, roomid }) => {
+    io.to(roomid).emit("who_joined", allusers);
+  });
+
+  // check connection
+  socket.on("msg", ({ data, roomid }) => {
+    io.to(roomid).emit("msg", data);
+  });
+
+  // get video state
+  socket.on("videoStates", ({ videoState, roomid }) => {
+    io.to(roomid).emit("videoStates", videoState);
+  });
+
+  // disconnect
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(port, () => {
